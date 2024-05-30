f5284527bbc874383be5b5018177f9b245eff073
Abishek shahi <128833491+AbishekJungThakuri@users.noreply.github.com>
2024-05-30 20:24:32 +0545

    Update 14_youtubeVideoDownloader.py

--- 14_youtubeVideoDownloader.py
+++ 14_youtubeVideoDownloader.py
@@ -1,31 +1,30 @@
 # ONly for single video
-
 from pytube import YouTube
 
-link = "https://www.youtube.com/watch?v=hWhyIwOXuGo"
+link = input("Enter the links of video: ")
 youtube_1 = YouTube(link)     # gets all the information of video
 
-# print(youtube_1.title)
+# print(youtube_1.title)  
 # print(youtube_1.thumbnail_url)
-
-video = youtube_1.streams.all()   # All format
+# video = youtube_1.streams.all()   # All format
 # video = youtube_1.streams.filter(only_audio=True)  # only for audio
+
+video = youtube_1.streams.filter(progressive=True)  #both audio and video
 
 vid = list(enumerate(video))   # gives with index number
 for i in vid:
     print(i)
 
 print()
-strm = int(input("Enter: "))
+strm = int(input("Enter the number which you want to download: "))
 video[strm].download()
 print("Successffully Downloaded!!!")
 
 
 #   *******Download all playlist videos*****
-
 # from pytube import Playlist
-
-# py = Playlist("https://www.youtube.com/playlist?list=PLeo1K3hjS3uu_n_a__MI_KktGTLYopZ12")
+# playlistLink = input("Enter the playlist link: ")
+# py = Playlist(playlistLink)
 
 # print(f'Downloading : {py.title}')
 
